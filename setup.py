92a2f50dd081a6c77e6948e00db56f99dcbb943f
Jeremy Blanchard <auzigog@gmail.com>
2011-11-11 12:59:40 -0800

    Fix proper package name in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='Fiesta Python API Wrapper',
-	version='1.0',
+	name='fiesta',
+	version='0.1',
 	description='Wrapper for Fiesta.cc API',
 	author='Fiesta API',
 	author_email='api@corps.fiesta.cc',
