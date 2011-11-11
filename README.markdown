20c12d7651aa540c85b90cac83ba83feac710aa0
Jeremy Blanchard <auzigog@gmail.com>
2011-11-11 13:31:26 -0800

    Update readme

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# fiesta-python
+
+Python wrapper for Fiesta's list management API.
+
+http://fiesta.cc
+
+http://docs.fiesta.cc
+
+
+
+## Installation
+The quickest way to installt his package is with pip:
+
+    pip install git://github.com/fiesta/fiesta-python.git
+
+## Usage
+
--- fiesta/fiesta.py
+++ fiesta/fiesta.py
@@ -1,34 +1,51 @@
-import base64, json, urllib2
+import base64
+import json
+import urllib2
 
 
-api_client_id = ""
-api_client_secret = ""
-basic_auth = base64.b64encode("%s:%s" % (api_client_id, api_client_secret))
+class FiestaAPI(object):
+    """
+    A Python wrapper to the Fiesta.cc API: http://docs.fiesta.cc/
+    """
+    api_url = "https://api.fiesta.cc/"
 
-def _create_and_send_request(uri, api_inputs):
-    request = urllib2.Request(uri)
-    request.add_header("Authorization", "Basic %s" % (basic_auth))
-    request.add_header("Content-Type", "application/json")
-    request.add_data(json.dumps(api_inputs))
+    client_id = None
+    client_secret = None
 
-    return urllib2.urlopen(request)
+    def __init__(self, client_id=None, client_secret=None):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def request(self, request_path, api_inputs=None):
+        request = urllib2.Request(uri)
+        basic_auth = base64.b64encode("%s:%s" % (self.client_id, self.client_secret))
+        request.add_header("Authorization", "Basic %s" % (basic_auth))
+        request.add_header("Content-Type", "application/json")
+        if api_inputs is not None:
+            request.add_data(json.dumps(api_inputs))
+
+        response = urllib2.urlopen(request)
+        return response
 
 
-def create_group_trusted():
-    create_group_uri = "https://api.fiesta.cc/group"
-
-    api_inputs = {}
-
-    response = _create_and_send_request(create_group_uri, api_inputs)
-    json_response = json.loads(response.read())
-
-    group_id = json_response['data']['group_id']
 
 
-def add_member_trusted(group_id, member_email, group_name):
-    add_member_uri = "https://api.fiesta.cc/membership/%s"
 
-    api_inputs = {'group_name': group_name,
-                  'address': member_email}
-
-    _create_and_send_request(add_member_uri % group_id, api_inputs)
+#def create_group_trusted():
+#    create_group_uri = "https://api.fiesta.cc/group"
+#
+#    api_inputs = {}
+#
+#    response = _create_and_send_request(create_group_uri, api_inputs)
+#    json_response = json.loads(response.read())
+#
+#    group_id = json_response['data']['group_id']
+#
+#
+#def add_member_trusted(group_id, member_email, group_name):
+#    add_member_uri = "https://api.fiesta.cc/membership/%s"
+#
+#    api_inputs = {'group_name': group_name,
+#                  'address': member_email}
+#
+#    _create_and_send_request(add_member_uri % group_id, api_inputs)
