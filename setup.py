707911e358a6221f0cb7c66678be38962365f427
Jeremy Blanchard <auzigog@gmail.com>
2011-11-11 12:48:04 -0800

    Add setup.py for quick pip installs.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='Fiesta Python API Wrapper',
+	version='1.0',
+	description='Wrapper for Fiesta.cc API',
+	author='Fiesta API',
+	author_email='api@corps.fiesta.cc',
+	url='https://github.com/fiesta/fiesta-python',
+)
